ea446a7014fdf19fd612455eb86d43e269188ed0
Tehila Stein <tstein@fieldwire.com>
2026-06-10 14:06:19 -0400

    - BIM-1304: Handle shared instance meshes

--- gltf/gltfpack.h
+++ gltf/gltfpack.h
@@ -65,6 +65,7 @@ struct Mesh
 
 	const char* parent_node_name;
 	size_t index_in_parent_node;
+	std::vector<const char*> node_parent_names;
 
 	std::vector<std::pair<const char*, unsigned int> >merged_meshes_parent_node_info;
 };
